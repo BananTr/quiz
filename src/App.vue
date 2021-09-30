be5f86b4285d6131db563e73c10a1d875ac87322
BananTr <nanooo_tair@hotmail.com>
2021-09-30 11:28:38 +0300

    Done

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,55 @@
 <template>
- <v-app id="app">
+  <v-app id="app">
     <v-container fluid fill-height>
       <v-layout align-center justify-center>
         <v-flex xs12 sm8 md4>
-          <Login />
+             <div role="alert" v-if="logged == false">
+                <Login v-on:userData="onclickdata" v-on:show="onclickshow"/>
+             </div>
+              <div role="alert" v-if="logged == true">
+              <p>ID : {{ pk }}</p>
+               <p>hello {{ username }}</p>
+               <p>Email :  {{ email }}</p>
+               <p>Staff : {{  is_staff }}</p>
+              
+             </div>
         </v-flex>
       </v-layout>
     </v-container>
- </v-app>
+  </v-app>
 </template>
 
 <script>
-import Login from './components/Login.vue'
-
+import Login from "./components/Login.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Login
+    Login,
   },
-  data(){
-    return{
-      
+  data() {
+    return {
+
+      logged: false,
+      show: true,
+      username: "",
+      email: "",
+      pk: 0,
+      is_staff: true,
+    };
+  },
+  methods: {
+    onclickdata (value) {
+      this.username = value.username
+      this.email = value.email
+      this.pk = value.pk
+      this.is_staff = value.is_staff
+    },
+
+    onclickshow (value) {
+      this.logged = value
     }
   },
-  methods:{
-    
-  }
-}
+};
 </script>
 
 <style>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,23 +1,116 @@
 <template>
   <div>
-    start
+    <v-card class="mx-auto" max-width="344">
+      <v-toolbar dark color="primary">
+        <v-toolbar-title>Login form </v-toolbar-title>
+      </v-toolbar>
+      <v-card-text>
+         <v-text-field
+          v-model="user.email"
+          :rules="[rules.required, rules.email]"
+          label="E-mail"
+        ></v-text-field>
+        <v-text-field
+          v-model="user.password"
+          :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+          :rules="[rules.required, rules.min]"
+          :type="show ? 'text' : 'password'"
+          name="input-10-1"
+          label="Password"
+          hint="At least 8 characters"
+          counter
+          @click:append="show = !show"
+        ></v-text-field>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          text
+          dark
+          color="primary"
+          v-on:click="loginSubmit()"
+          v-on:keyup="loginSubmit()"
+        >
+          Login
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+    <div>
+      <v-snackbar v-model="snackbar">
+        {{ text }}
+        <template v-slot:action="{ attrs }">
+          <v-btn color="pink" text v-bind="attrs" @click="snackbar = false">
+           
+          </v-btn>
+        </template>
+      </v-snackbar>
+      
+    </div>
   </div>
 </template>
 
 <script>
-
+import axios from "axios";
 export default {
-  name: 'Login',
-  data(){
-    return{
-    }
+  name: "Login",
+
+  data() {
+    return {
+      user: {
+        password: "",
+        email: "",
+      },
+      key: "",
+      show: false,
+      snackbar: false,
+      text: "",
+      rules: {
+        required: (value) => !!value || "Required",
+        min: (v) => v.length >= 8 || "Min 8 characters",
+        emailMatch: () => `The email and password you entered don't match`,
+        email: (value) => {
+          const pattern =
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          return pattern.test(value) || "Invalid e-mail.";
+        },
+      },
+    };
   },
-  methods:{
-    
-  }
-}
+  methods: {
+    async loginSubmit() {
+      await axios
+        .post(
+          `https://v2202107122785158474.goodsrv.de/api/rest-auth/login/`,
+          this.user
+        )
+        .then((response) => {
+          console.log(response);
+          if (response.status == 200) {
+            this.text = `You have loged in sucssfully`;
+            this.snackbar = true;
+            let key = response.data.key;
+            axios
+              .get(
+                "https://v2202107122785158474.goodsrv.de/api/rest-auth/user/",
+                {
+                  headers: { Authorization: `Token ${key}` },
+                }
+              )
+              .then((response) => {
+                this.$emit("userData", response.data);
+                this.$emit("show", true);
+              });
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          this.text = `Error`;
+          this.snackbar = true;
+        });
+    },
+  },
+};
 </script>
-
 <style scoped>
-
 </style>
+
